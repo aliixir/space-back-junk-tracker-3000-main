3c7de65e2989b797d80b1a0c1b36c6bba49cb751
alexisolaess <olaes.alexis@gmail.com>
2021-10-12 23:23:53 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+// Import any Lit elements here
+import './components/my-element.js';
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
